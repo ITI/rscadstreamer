1dec914acee3c4a5e0e79037fe23fdfe7f56ef3d
jmj <jeremyj@letifer.org>
2012-02-07 14:18:20 -0600

    added installer

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='rscadstreamer',
+        autho='Jeremy Jones',
+        autho_email='jmjone@illinois.edu',
+        url='http://code.iti.illinois.edu/redmine/projects/rscadstreamer',
+        version='1.0',
+        packages=['rtds'],
+        scripts=['rscadstreamer'],
+    )
