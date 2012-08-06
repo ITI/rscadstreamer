3620b25a67739bc21afad691ceacf7e37ced015f
Jeremy Jones <jmjone@illinois.edu>
2012-08-06 16:33:58 -0500

    add argparse to the setup requirements

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setup(
         author_email='jmjone@illinois.edu',
         url='http://code.iti.illinois.edu/redmine/projects/rscadstreamer',
         version='1.0',
+        requires=[
+            'argparse',
+        ],
         packages=['rtds'],
         scripts=['rscadstreamer'],
     )
