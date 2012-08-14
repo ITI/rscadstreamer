e17e8c5bd4b9e072ba2828dd8f9528aea0695096
Jeremy Jones <jmjone@illinois.edu>
2012-08-14 15:21:18 -0500

    testing of rtds interaction needed for deft

---
+++ plugins/deft.py
@@ -0,0 +1,24 @@
+import sys
+
+from rtds.rscadplugin import RSCADPlugin
+from rtds.rscadutils import debug
+
+class deft(RSCADPlugin):
+    def init(self, args):
+        debug('DEFT: init()')
+
+        self.sw_state = 0
+
+        return {
+            'commands': {'deft:switch': self.cmd_sw_toggle},
+        }
+
+    def cmd_sw_toggle(self, cmd, rscad):
+
+        if self.sw_state:
+            newstate = 0
+        else:
+            newstate = 1
+        debug('cmd_sw_toggle(): {0} --> {1}'.format(self.sw_state, newstate))
+        rscad.send('SetSwitch "sw1" = {0};'.format(newstate))
+        self.sw_state = newstate
