ac76f3e70adad86a8750d461cdb1497b363d3e16
Jeremy Jones <jmjone@illinois.edu>
2013-10-24 12:41:59 -0500

    tweaks for new breaker control

--- plugins/autobreak.py
+++ plugins/autobreak.py
@@ -7,10 +7,14 @@ class autobreak(RSCADPlugin):
 
     values = [
             {
-                'P_GEN1' : '0.54',   'V_GEN1' : '1.061',
-                'P_GEN2' : '0.4135', 'V_GEN2' : '1.051',
-                'P_GEN3' : '0.0135', 'V_GEN3' : '1.05077',
-                'P_GEN4' : '0.72',   'V_GEN4' : '1.009',
+                'P_GEN1' : '0.41',   'V_GEN1' : '1.061',
+                'P_GEN2' : '0.8',    'V_GEN2' : '1.051',
+                'P_GEN3' : '0.54',   'V_GEN3' : '1.05077',
+                'P_GEN4' : '0.55',   'V_GEN4' : '1.009',
+            },
+            {
+                'P_GEN1' : '0.54', 'P_GEN2' : '0.4135',
+                'P_GEN3' : '0.0135','P_GEN4' : '0.72'
             },
             {'P_GEN1' : '0.61'},
             {'P_GEN4' : '0.76'},
@@ -43,11 +47,21 @@ class autobreak(RSCADPlugin):
     def do_timer(self, watcher, events):
         debug('Timer fired')
 
+        print('Setting sequence {0}'.format(self.current))
+
         rscad = watcher.loop.data
+
         for k,v in self.values[self.current].items():
             rscad.send('SetSlider "{0}" = {1};'.format(k, v))
         rscad.send('PushButton "SeqTrigger";')
         rscad.send('ReleaseButton "SeqTrigger";')
+
+        if self.current == 0:
+            # make sure the breaker are reset
+            pyev.sleep(2)
+            rscad.send('PushButton "CLOSE";')
+            rscad.send('ReleaseButton "CLOSE";')
+
 
         if self.current == len(self.values) - 1:
             debug('Resetting timer to 30 seconds')
