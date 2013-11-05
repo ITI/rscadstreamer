539ac38435d597eae2b7318b94691544fb8a1729
Jeremy Jones <jmjone@illinois.edu>
2013-11-05 10:28:41 -0600

    update logic to handle single run and longer initial state

--- plugins/autobreak.py
+++ plugins/autobreak.py
@@ -12,38 +12,10 @@ class autobreak(RSCADPlugin):
                 'P_GEN3' : '0.4135',
                 'P_GEN4' : '0.72',
             },
-            {
-                'P_GEN1' : '0.61',
-            },
-            {
-                'P_GEN2' : '0.3435',
-            },
-            {
-                'P_GEN1' : '0.75',
-            },
-        ]
-
-
-
-
-
-#    values = [
-#            {
-#                'P_GEN1' : '0.41',   'V_GEN1' : '1.061',
-#                'P_GEN2' : '0.8',    'V_GEN2' : '1.051',
-#                'P_GEN3' : '0.54',   'V_GEN3' : '1.05077',
-#                'P_GEN4' : '0.55',   'V_GEN4' : '1.009',
-#            },
-#            {
-#                'P_GEN1' : '0.54', 'P_GEN2' : '0.4135',
-#                'P_GEN3' : '0.0135','P_GEN4' : '0.72'
-#            },
-#            {'P_GEN1' : '0.61'},
-#            {'P_GEN4' : '0.76'},
-#            {'P_GEN2' : '0.3435'},
-#            {'P_GEN1' : '0.65'},
-#            {'P_GEN3' : '0.4'},
-#        ]
+            {'P_GEN1' : '0.61'},
+            {'P_GEN2' : '0.3435'},
+            {'P_GEN1' : '0.75'},
+    ]
 
     def options(self, parser):
         debug('options')
@@ -56,7 +28,7 @@ class autobreak(RSCADPlugin):
         debug('init')
 
         l = pyev.default_loop()
-        self.timer = pyev.Timer(0.0, 30.0, l, self.do_timer)
+        self.timer = pyev.Timer(30.0, 30.0, l, self.do_timer)
         self.timer.start()
 
         return {
@@ -78,20 +50,21 @@ class autobreak(RSCADPlugin):
         rscad.send('PushButton "SeqTrigger";')
         rscad.send('ReleaseButton "SeqTrigger";')
 
-        if self.current == 0:
-            # make sure the breaker are reset
-            pyev.sleep(2)
-            rscad.send('PushButton "CLOSE";')
-            rscad.send('ReleaseButton "CLOSE";')
+        #if self.current == 0:
+        #    # make sure the breaker are reset
+        #    pyev.sleep(2)
+        #    rscad.send('PushButton "CLOSE";')
+        #    rscad.send('ReleaseButton "CLOSE";')
 
 
         if self.current == len(self.values) - 1:
-            debug('Resetting timer to 30 seconds')
-            watcher.repeat = 60.0
-            self.current = 0
+            debug('Completed sequence')
+            watcher.stop()
+            return
+            #watcher.repeat = 60.0
+            #self.current = 0
         else:
             debug('Leaving timer at 10 seconds')
-            watcher.repeat = 30.0
+            watcher.repeat = 15.0
             self.current +=1
-
-        watcher.reset()
+            watcher.reset()
